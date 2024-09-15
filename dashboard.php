088b520c5b2098f0cb7ff7e11cc15fea2fabd821
Titanbiru <titaniumehe1@gmail.com>
2024-09-15 19:10:32 +0700

    memperbaiki bug di master

--- dashboard.php
+++ dashboard.php
@@ -33,7 +33,7 @@ $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="dasboard-1.css">
+    <link rel="stylesheet" href="dashboard-1.css">
 </head>
 <body>
     <?php include "layout/header.html"?>
