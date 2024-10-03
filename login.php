b2bdcb404876d03d824981fdf456b8769a00da12
Titanbiru <titaniumehe1@gmail.com>
2024-10-03 08:18:43 +0700

    fixing bug for login and index

--- login.php
+++ login.php
@@ -8,16 +8,17 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="css/login-1.css">
+    <link rel="stylesheet" href="css/login-2.css">
 </head>
 <body>
-    <h2>Login</h2>
-    <form method="POST" action="login_process.php">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
+    <div class="container">
+        <h2>Login</h2>
+        <form method="POST" action="login_process.php">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+            
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
         <?php if (isset($_SESSION['login_error'])): ?>
         <p class="error-message"><?php echo htmlspecialchars($_SESSION['login_error']); ?></p>
         
@@ -25,6 +26,7 @@ session_start();
         <button type="submit">Login</button>
         <p>Don't have an account? <a href="register.php">Register here</a></p>
     </form>
+    </div>  
 </body>
 </html>
 
