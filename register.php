5e4beb5a1a8fe1d2bc0b4ed86b7f2230021ccd6a
konan26 <raykonanr@gmail.com>
2024-10-03 07:42:59 +0700

    menambahkan code css dari file register

--- register.php
+++ register.php
@@ -8,20 +8,25 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="regis-1.css">
+    <link rel="stylesheet" href="css/regis-2.css">
 </head>
-<body>
+<body class="body">
+    <div class="container">
     <h2>Register</h2>
     
     <form method="POST" action="register_process.php">
+    <div class="form-group">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required>
-
+    </div>
+    <div class="form-group">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required>
-
+    </div>
+    <div class="form-group">
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required>
+        </div>
         <?php if (isset($_SESSION['register_error'])): ?>
             <p class="error-message"><?php echo htmlspecialchars($_SESSION['register_error']); ?></p>
         <?php endif; ?>
